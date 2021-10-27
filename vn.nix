3b906db341ca163dfafe849ac44e327b84f85d8e
Phạm Văn Dũng <dungph@zoho.com>
2021-10-27 17:38:06 +0700

    move stuff

--- vn.nix
+++ vn.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }:
 {
+  # inputMethod
   i18n.inputMethod.enabled = "ibus";
   i18n.inputMethod.ibus.engines = with pkgs.ibus-engines; [ bamboo ];
+
+  # Set your time zone.
+  time.timeZone = "Asia/Ho_Chi_Minh";
 } 
