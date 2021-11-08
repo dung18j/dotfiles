8fe3a00375f3d306d2a527220d0d8cc4b039f1be
Phạm Văn Dũng <dungph@zoho.com>
2021-11-08 22:55:02 +0700

    move nvim, system config to config.nix

--- config.nix
+++ config.nix
@@ -5,12 +5,35 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-      ./nvim/nvim.nix
-  ];
+
   
-  # Allow Unfree software
-  nixpkgs.config.allowUnfree = true;
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "thinkpad"; # Define your hostname.
+  networking.networkmanager.enable = true;  # Enables network support via networkmanager.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp4s0.useDHCP = true;
+  networking.interfaces.wlp5s0.useDHCP = true;
+
+  # Default editor
+  environment.variables.EDITOR = "nvim";
+
+  # neovim-nightly-overlay
+  nixpkgs.overlays = [
+    (import (builtins.fetchTarball {
+      url = https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz;
+    }))
+  ];
+ 
+   # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
 
   # inputMethod
   i18n.inputMethod.enabled = "ibus";
@@ -26,6 +49,10 @@
   # Set your time zone.
   time.timeZone = "Asia/Ho_Chi_Minh";
   
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.dungph = {
     isNormalUser = true;
@@ -77,6 +104,9 @@
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
 
+  # Allow Unfree software
+  nixpkgs.config.allowUnfree = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
@@ -117,7 +147,64 @@
     transmission
     gimp
     obs-studio
+
+    # Neovim
+    (neovim.override {
+      viAlias = true;
+      vimAlias = true;
+      configure = {
+        customRC = ''
+            luafile /home/dungph/.dotfiles/nvim/settings.lua
+        '';
+        packages.myVimPackage = with pkgs.vimPlugins; {
+          # loaded on launch
+          start = [ 
+            # ident vertical line
+            indentLine
+
+            # auto complete
+            nvim-cmp
+            cmp-nvim-lsp
+            cmp-path
+            cmp-buffer
+            cmp-vsnip
+            vim-vsnip
+
+            # Lsp stuff
+            vim-nix
+            nvim-lspconfig
+            rust-tools-nvim
+            popup-nvim
+            plenary-nvim
+            telescope-nvim
+            nvim-dap
+            lspsaga-nvim
+            rust-vim
+
+            # color
+            nord-vim
+            nvim-treesitter
+
+            # nvim tree
+            nvim-web-devicons 
+            nvim-tree-lua
+
+            # markdown preview
+            glow-nvim
+          ];
+          # manually loadable by calling `:packadd $plugin-name`
+          opt = [ ];
+        };
+      };
+    })
   ];
 
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "21.05"; # Did you read the comment?
 }
 
