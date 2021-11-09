f8384894e6d053045b19f4676ffa51d5b6f2036c
Phạm Văn Dũng <dungph@zoho.com>
2021-11-09 10:18:19 +0700

    stop using treesetter

--- nvim/settings.lua
+++ nvim/settings.lua
@@ -43,12 +43,12 @@ opt.lazyredraw = true;
 opt.shell = "fish"
 opt.shadafile = "NONE"
 
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    highlight = {
-        enable = true,              -- false will disable the whole extension
-    },
-    autotag = {
-        enable = true,
-    },
-}
+--require'nvim-treesitter.configs'.setup {
+--    ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+--    highlight = {
+--        enable = true,              -- false will disable the whole extension
+--    },
+--    autotag = {
+--        enable = true,
+--    },
+--}
