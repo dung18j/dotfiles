42fce191f192bdd8454ab35dca237bfe3e322bda
Phạm Văn Dũng <dungph@zoho.com>
2021-12-17 17:34:42 +0700

    ident

--- nvim/tree.lua
+++ nvim/tree.lua
@@ -57,44 +57,44 @@ vim.g.nvim_tree_icons = {
 }
 
 require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = true,
-  ignore_ft_on_setup  = {},
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
-  auto_close          = false,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  diagnostics         = {
-    enable = true,
-    icons = {
-      hint = "H",
-      info = "I",
-      warning = "W",
-      error = "E",
+    disable_netrw       = true,
+    hijack_netrw        = true,
+    open_on_setup       = true,
+    ignore_ft_on_setup  = {},
+    update_to_buf_dir   = {
+        enable = true,
+        auto_open = true,
+    },
+    auto_close          = false,
+    open_on_tab         = false,
+    hijack_cursor       = false,
+    update_cwd          = false,
+    diagnostics         = {
+        enable = true,
+        icons = {
+            hint = "H",
+            info = "I",
+            warning = "W",
+            error = "E",
+        }
+    },
+    update_focused_file = {
+        enable      = false,
+        update_cwd  = false,
+        ignore_list = {}
+    },
+    system_open = {
+        cmd  = nil,
+        args = {}
+    },
+    view = {
+        width = 30,
+        height = 30,
+        side = 'left',
+        auto_resize = false,
+        mappings = {
+            custom_only = false,
+            list = {}
+        }
     }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  view = {
-    width = 30,
-    height = 30,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    }
-  }
 }
