a1cf5c196cc93b485b94324f7af11ef0a804f5e5
Phạm Văn Dũng <dung18j@gmail.com>
2022-01-03 00:14:06 +0700

    grouping setup scripts

---
+++ setup-scripts/packages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+sudo pacman -S\
+    `#Essential`\
+    alacritty\
+    tmux\
+    fish\
+    firefox\
+    neovim\
+    `# Programming`\
+    lua-language-server\
+    rust-analyzer\
+    docker\
+    docker-compose\
+    rustup\
+    `#Nice to have`\
+    ttf-fira-sans\
+    ttf-fira-code\
+    exa\
+    bat\
+    transmission-cli\
+    ripgrep\
+    wl-clipboard\
+    xclip\
+    dnsmasq\
+    gimp\
+    htop
+
+yay -S\
+    teams\
+    dropbox\
+    ibus-bamboo
