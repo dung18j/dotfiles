57506079e7f0805c9bc3a501abf414609dbf31b5
Phạm Văn Dũng <dung18j@gmail.com>
2022-01-05 16:10:58 +0700

    use C-a to open terminal

--- nvim/utils.lua
+++ nvim/utils.lua
@@ -8,8 +8,8 @@ require'FTerm'.setup({
 -- Example keybindings
 local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
-map('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
-map('t', '<C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
+map('n', '<C-a>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
+map('t', '<C-a>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
 
 require'nvim-treesitter.configs'.setup {
     ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
