fa39fc37720832b4c06388a69b41490520bd2bd8
Phạm Văn Dũng <dung18j@gmail.com>
2022-03-23 11:12:12 +0700

    add light theme and switcher

--- fish/config.fish
+++ fish/config.fish
@@ -2,13 +2,12 @@ abbr l exa
 abbr ls exa
 abbr la "exa -a"
 abbr ll "exa -l"
-abbr cat bat
 abbr vim nvim
 abbr vi nvim
 abbr rmr "rm -r"
 abbr rmrf "rm -rf"
-abbr dark_theme "gsettings set org.gnome.desktop.interface gtk-theme HighContrastInverse&&git -C ~/.config/ checkout dark-theme"
-abbr light_theme "gsettings set org.gnome.desktop.interface gtk-theme HighContrast&&git -C ~/.config/ checkout light-theme"
+alias dark_theme "gsettings set org.gnome.desktop.interface gtk-theme HighContrastInverse &&sed -i -e 's/light/dark/' ~/.config/nvim/init.lua &&sed -i -e 's/*light/*dark/' ~/.config/alacritty/alacritty.yml"
+alias light_theme "gsettings set org.gnome.desktop.interface gtk-theme HighContrast&&sed -i -e 's/dark/light/' ~/.config/nvim/init.lua &&sed -i -e 's/*dark/*light/' ~/.config/alacritty/alacritty.yml"
 
 set fish_prompt_pwd_dir_length 3
 setenv EDITOR nvim
