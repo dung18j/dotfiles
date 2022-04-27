ae090273196aadbe24439b5c99e43bb030aae800
Phạm Văn Dũng <dung18j@gmail.com>
2022-04-27 09:17:32 +0700

    move utils to init

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,7 @@ dofile(user_folder .. "/.config/nvim/plugins.lua")
 dofile(user_folder .. "/.config/nvim/language.lua")
 dofile(user_folder .. "/.config/nvim/cmp.lua")
 dofile(user_folder .. "/.config/nvim/tree.lua")
-dofile(user_folder .. "/.config/nvim/utils.lua")
+--dofile(user_folder .. "/.config/nvim/utils.lua")
 
 -- Nicer UI settings
 vim.opt.cursorline = true
@@ -33,6 +33,7 @@ vim.opt.clipboard = 'unnamedplus'
 
 -- Colorscheme: gruvbox, medium dark
 vim.cmd([[
+    let g:everforest_transparent_background = 1
     if has('termguicolors')
         set termguicolors
     endif
@@ -45,3 +46,34 @@ local map = vim.api.nvim_set_keymap
 map('n', 'q:', '<nop>', {noremap = true})
 map('n', 'q', '<nop>', {noremap = true})
 map('n', 'Q', 'q', {noremap = true})
+
+
+require'FTerm'.setup({
+    border = 'double',
+    dimensions  = {
+        height = 0.9,
+        width = 0.9,
+    },
+})
+
+-- Example keybindings
+local opts = { noremap = true, silent = true }
+map('n', '<C-a>', '<CMD>lua require("FTerm").toggle()<CR>', opts)
+map('t', '<C-a>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
+
+require'nvim-treesitter.configs'.setup {
+    ensure_installed = {'rust', 'lua', 'toml', 'json', 'c', 'java', 'html'},
+    highdark = {
+        enable = true,              -- false will disable the whole extension
+    },
+    autotag = {
+        enable = true,
+    },
+}
+
+require('nvim-autopairs').setup{}
+
+-- ident characters
+vim.g.indentLine_char_list = {'▏', '╎', '┆', '┊'}
+
+vim.cmd("autocmd FileType * setlocal foldmethod=manual")
