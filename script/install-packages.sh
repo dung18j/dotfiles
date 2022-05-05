23e70626df9fe6e0f6b544a30af470890e97e7f3
Phạm Văn Dũng <dung18j@gmail.com>
2022-05-05 22:34:15 +0700

    separate script

---
+++ script/install-packages.sh
@@ -0,0 +1,27 @@
+sudo pacman --noconfirm -S\
+    alacritty\
+    tmux\
+    fish\
+    firefox\
+    neovim\
+    lua-language-server\
+    rust-analyzer\
+    docker\
+    docker-compose\
+    rustup\
+    yarn\
+    dbeaver\
+    postgresql\
+    sqlx-cli\
+    ttf-fira-sans\
+    ttf-fira-code\
+    noto-fonts-cjk\
+    exa\
+    bat\
+    transmission-cli\
+    ripgrep\
+    wl-clipboard\
+    xclip\
+    dnsmasq\
+    gimp\
+    htop
---
+++ script/uninstall-gnome-unused.sh
@@ -0,0 +1,10 @@
+sudo pacman --noconfirm -R\
+    cheese\
+    totem\
+    gnome-books\
+    gnome-maps\
+    gnome-music\
+    gnome-weather\
+    gnome-software\
+    epiphany\
+    gnome-terminal
