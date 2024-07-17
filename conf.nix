57fc31bee15b54a4200d30ded3fd08171e169077
Phạm Văn Dũng <dung18j@gmail.com>
2024-07-17 07:30:18 +0700

    add tlp

--- conf.nix
+++ conf.nix
@@ -42,12 +42,32 @@
 
   time.timeZone = "Asia/Ho_Chi_Minh";
 
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
+  sound.enable = true;
   hardware.opengl.enable = true;
+  #security.rtkit.enable = true;
 
-  security.rtkit.enable = true;
+  services.power-profiles-daemon.enable = false;  
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
   
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+  
+     #Optional helps save long term battery health
+     START_CHARGE_THRESH_BAT0 = 70; # 40 and bellow it starts to charge
+     STOP_CHARGE_THRESH_BAT0 = 75; # 80 and above it stops charging
+  
+    };
+  };  
 
   system.stateVersion = "24.05"; # Did you read the comment?
 }
